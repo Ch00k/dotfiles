fc1f9c368c138cf84b2ae7b6b62a34a2d50231a5
Andrii Yurchuk <>
2021-11-24 07:59:54 +0100

    Some updates

--- other/.hammerspoon/init.lua
+++ other/.hammerspoon/init.lua
@@ -1,5 +1,43 @@
-local wf = hs.window.filter
-wf_alacritty = wf.new{'Alacritty'}
-wf_alacritty:subscribe(wf.windowFocused, function(window, title)
-    hs.keycodes.setLayout('U.S.')
-end)
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
+
+local soco = '/opt/homebrew/bin/soco'
+local speaker = 'Office'
+local volumeLow = '3'
+local volumeHigh = '9'
+
+function _() end
+
+function volumeUp()
+    hs.task.new(soco, nil, _, {speaker, 'volume', volumeHigh}):start()
+end
+
+function volumeDown()
+    hs.task.new(soco, nil, _, {speaker, 'volume', volumeLow}):start()
+end
+
+function play()
+    hs.task.new(soco, nil, _, {speaker, 'play'}):start()
+end
+
+function pause()
+    hs.task.new(soco, nil, _, {speaker, 'pause'}):start()
+end
+
+function sleepWatch(eventType)
+    if (eventType == hs.caffeinate.watcher.systemWillSleep) then
+        pause()
+    elseif (eventType == hs.caffeinate.watcher.systemDidWake) then
+        play()
+    elseif (eventType == hs.caffeinate.watcher.screensDidLock) then
+        volumeDown()
+    elseif (eventType == hs.caffeinate.watcher.screensDidUnlock) then
+        volumeUp()
+    end
+end
+
+sleepWatcher = hs.caffeinate.watcher.new(sleepWatch):start()
+
+zoom_meeting = hs.window.filter.new(false):setAppFilter('zoom.us', {allowTitles={'Zoom Meeting', 'zoom share'}})
+zoom_meeting:subscribe(hs.window.filter.windowCreated, volumeDown)
+zoom_meeting:subscribe(hs.window.filter.windowDestroyed, volumeUp)
